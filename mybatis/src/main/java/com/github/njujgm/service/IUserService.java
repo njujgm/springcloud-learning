b303b9576c3e4b0c6a529599808297485318cf40
njujgm <njujgm@gmail.com>
2021-07-30 21:42:17 +0800

    修改版本

---
+++ mybatis/src/main/java/com/github/njujgm/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.github.njujgm.service;
+
+import com.github.njujgm.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author jugaoming
+ * @since 2021-06-29
+ */
+public interface IUserService extends IService<User> {
+
+}
